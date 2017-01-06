f214a19f60e672b4e487ee2ff09de407f0e13e11
Sebastian Köln <sebk@rynx.org>
2017-01-06 13:50:15 +0100

    added examples

--- docs/editor.js
+++ docs/editor.js
@@ -1,8 +1,12 @@
-let worker = new Worker('worker.js');
+let input_element;
+let worker;
+let svg_element;
+let nav;
+
+worker = new Worker('worker.js');
 worker.addEventListener('message',
     function(e) {
-        let svg = document.getElementById("view-svg");
-        svg.innerHTML = e.data.svg;
+        svg_element.innerHTML = e.data.svg;
     }
 );
 function send_input(input) {
@@ -12,20 +16,41 @@ function send_input(input) {
     });
 }
 
+function input_from_element(e) {
+    let input = e.target.latex;
+    input_element.value = input;
+    send_input(input);
+}
+
 function update_input(e) {
     let input = document.getElementById("input").value;
     send_input(input);
 }
 
 document.addEventListener("DOMContentLoaded", function(e) {
-    let input = document.getElementById("input");
-    input.addEventListener("change", update_input);
+    svg_element = document.getElementById("view-svg");
+    nav = document.getElementById("nav");
+    input_element = document.getElementById("input");
+    
+    var examples = new Request("examples.json");
+    fetch(examples)
+    .then(response => response.json())
+    .then(function(json) {
+        for (var ex of json) {
+            let p = document.createElement("p");
+            p.setAttribute("title", ex.title);
+            p.latex = ex.latex;
+            p.addEventListener("click", input_from_element);
+            nav.appendChild(p);
+        }
+    });
+    
+    input_element.addEventListener("change", update_input);
     update_input();
 });
 
 let zoom_level = 0;
 function zoom(level) {
     zoom_level += level;
-    let svg = document.getElementById("view-svg");
-    svg.style.zoom = Math.pow(1.3, zoom_level);
+    svg_element.style.zoom = Math.pow(1.3, zoom_level);
 }
